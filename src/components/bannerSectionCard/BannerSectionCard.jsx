4ca815e67f0be35068d10479311a1560f6d8ade1
tfshorifulislam <tfshorifulislam@gmail.com>
2026-04-16 00:57:21 +0600

    banner card update

--- src/components/bannerSectionCard/BannerSectionCard.jsx
+++ src/components/bannerSectionCard/BannerSectionCard.jsx
@@ -6,19 +6,19 @@ const BannerSectionCard = () => {
     return (
         <div className='mx-auto w-11/12'>
             <div className='grid grid-cols-2 md:grid-cols-4 gap-3 md:gap-6 mt-10 mb:mt-20 '>
-                <div className='text-center py-8 px-3 bg-white shadow-lg'>
+                <div className='text-center py-8 px-3 bg-white shadow-lg rounded-lg'>
                     <p className='text-[#244D3F] mb-2 font-semibold text-3xl'>{friendsData.length}</p>
                     <p className='text-[#64748B] text-lg'>Total Friends</p>
                 </div>
-                <div className='text-center py-8 px-3 bg-white shadow-lg'>
+                <div className='text-center py-8 px-3 bg-white shadow-lg rounded-lg'>
                     <p className='text-[#244D3F] mb-2 font-semibold text-3xl'>{friendsData.filter(f => f.status === 'on-track').length}</p>
                     <p className='text-[#64748B] text-lg'>On Track</p>
                 </div>
-                <div className='text-center py-8 px-3 bg-white shadow-lg'>
+                <div className='text-center py-8 px-3 bg-white shadow-lg rounded-lg'>
                     <p className='text-[#244D3F] mb-2 font-semibold text-3xl'>6</p>
                     <p className='text-[#64748B] text-lg'>Need Attention</p>
                 </div>
-                <div className='text-center py-8 px-3 bg-white shadow-lg'>
+                <div className='text-center py-8 px-3 bg-white shadow-lg rounded-lg'>
                     <p className='text-[#244D3F] mb-2 font-semibold text-3xl'>{friendsData.filter(f => f.days_since_contact <= 30).length}</p>
                     <p className='text-[#64748B] text-lg'>Interactions This Month</p>
                 </div>
