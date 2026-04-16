dd176a1fefe3d6cef21cfcceea4ce1c90fabade1
tfshorifulislam <tfshorifulislam@gmail.com>
2026-04-17 03:10:45 +0600

    stats page full function and design  done

--- src/pages/Stats/Stats.jsx
+++ src/pages/Stats/Stats.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { Cell, Label, Legend, Pie, PieChart, Tooltip } from 'recharts';
 import { friendDataContext } from '../../friendContext/FriendContext';
 
@@ -18,34 +18,40 @@ const Stats = () => {
     const COLORS = ['#0088FE', '#00C49F', '#FF8042'];
 
     return (
-        <div className='flex justify-center items-center'>
-            <PieChart
-                style={{ width: '100%', maxWidth: '500px', maxHeight: '80vh', aspectRatio: 1 }}
-                responsive
-                margin={{
-                    top: 50,
-                    right: 120,
-                    bottom: 0,
-                    left: 120,
-                }}
-            >
-                <Pie
-                    data={data}
-                    cx="50%"
-                    cy="50%"
-                    innerRadius={60}
-                    outerRadius={80}
-                    dataKey="value"
-                >
-                    {
-                        data.map((entry, index) => (
-                            <Cell key={`cell-${index}`} fill={COLORS[index]} />
-                        ))
-                    }
-                </Pie>
-                <Tooltip />
-                <Legend />
-            </PieChart>
+        <div className='mt-20 w-10/12 mx-auto'>
+
+            <div className='flex justify-center m-5 font-bold text-[#1f2937] text-xl md:text-4xl'>
+                <p>Friendship Analytics</p>
+            </div>
+
+            <div className='bg-white py-10 shadow-sm rounded-xl'>
+                <div className='font-medium text-sm md:text-xl text-[#244d3f] flex justify-center'>
+                    <p>By Interaction Type</p>
+                </div>
+
+                <div className='flex justify-center'>
+                    <PieChart
+                        style={{ width: '100%', maxWidth: '100%', maxHeight: '30vh', aspectRatio: 1 }}
+                        responsive
+                    >
+
+                        <Pie
+                            data={data}
+                            innerRadius={60}
+                            outerRadius={80}
+                            dataKey="value"
+                        >
+                            {
+                                data.map((entry, index) => (
+                                    <Cell key={`cell-${index}`} fill={COLORS[index]} />
+                                ))
+                            }
+                        </Pie>
+                        <Tooltip />
+                        <Legend />
+                    </PieChart>
+                </div>
+            </div>
         </div>
     );
 };
